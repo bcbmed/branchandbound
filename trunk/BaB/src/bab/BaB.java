e99028e6c50d17040779deef8a2f5c1b1e4806fd
guigzrio@gmail.com <guigzrio@gmail.com@5e214bb9-23b2-fd98-ca74-629452c1ef63>
2015-01-24 16:35:18 +0000

    Bab

--- trunk/BaB/src/bab/BaB.java
+++ trunk/BaB/src/bab/BaB.java
@@ -228,5 +228,26 @@ public class BaB {
     public static void main(String[] args) {
        BaB bab = new BaB(3);
        bab.launchSimulator();
+       
+//            /* Total number of processors or cores available to the JVM */
+//       System.out.println("Available processors (cores): " + 
+//           Runtime.getRuntime().availableProcessors());
+//
+//       /* Total amount of free memory available to the JVM */
+//       System.out.println("Free memory (bytes): " + 
+//           Runtime.getRuntime().freeMemory());
+//
+//       /* This will return Long.MAX_VALUE if there is no preset limit */
+//       long maxMemory = Runtime.getRuntime().maxMemory();
+//       /* Maximum amount of memory the JVM will attempt to use */
+//       System.out.println("Maximum memory (bytes): " + 
+//           (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+//
+//       /* Total memory currently available to the JVM */
+//       System.out.println("Total memory available to JVM (bytes): " + 
+//           Runtime.getRuntime().totalMemory());
+//       
+//        System.out.println("OS : " +
+//                System.getProperty("os.name") + " " + System.getProperty("os.arch"));
     }    
 }
